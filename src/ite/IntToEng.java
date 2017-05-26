06c6d00c3819d50cb649ce4a8eaf04e431018ca3
g15916ka <g15916ka@s30405.tsuda.ac.jp>
2017-05-26 10:40:44 +0900

    999までの数字を英語に変換できるようにした。

--- src/ite/IntToEng.java
+++ src/ite/IntToEng.java
@@ -7,16 +7,54 @@ public class IntToEng {
 
         Scanner sc = new Scanner(System.in);
         int input = sc.nextInt();
+        if(input == 0){
+        	System.out.println("zero");        	
+        }else{
         System.out.println(translateEng(input));
+        }
 
     }
 
-    // 数値を英訳する変換するメソッド
+    // 3桁までの数値を英訳する変換するメソッド
     static String translateEng(int n) {
     	StringBuffer sb = new StringBuffer();
-    	sb.append(number(n%10));
-    	sb.append(" ");
-    	sb.append(number10(n/10));
+    	//100の位を定義
+    	//100の位が0でないときに　"○ hundred(s)"を表示
+    	if(n/100 != 0){
+    		sb.append(number(n/100));//100の位
+    		sb.append(" ");//100の位とhundredの間のスペース 
+    		sb.append(number100(n/100));//hundredかhundreds
+    	}
+    	//10の位が0でないときに 10の位を定義
+    	if(n>=100){ //nが3桁のとき
+    		if(n%10 <=19 && n %10 >= 11){//11から19のとき
+    			sb.append(" ");
+    			sb.append(number11To19(n));
+    		}else if(n % 100 ==0){
+    			sb.append("");
+    		}else{//11から19以外のとき
+    			sb.append(" ");
+    			sb.append(number10(n/10));//10の位	
+    			if(n%10 != 0) {
+    				sb.append(" ");//10の位と1の位の間のスペース
+    				sb.append(number(n%10));//1の位
+    			}
+    		}
+    	}
+    	if(n<=99 && n>=10){//nが2桁のとき
+    		if(n <=20 && n  >= 10){
+    			sb.append(number11To19(n));
+    		}else{
+    			sb.append(number10(n/10));//10の位	
+    			if(n%10 != 0) {
+    				sb.append(" ");//10の位と1の位の間のスペース
+    				sb.append(number(n%10));//1の位
+    			}
+    		}
+    	}
+    	if(n<=9){
+    		sb.append(number(n%10));
+    	}
         return new String(sb);
     }
     
@@ -44,25 +82,55 @@ public class IntToEng {
     }
     
     static String number10(int n) {
-    	if(n == 1) {
+    	if(n%10 == 1) {
     		return "ten";
-    	}if(n == 2) {
+    	}if(n%10 == 2) {
     		return "twenty";
-    	}if(n == 3) {
+    	}if(n%10 == 3) {
     		return "thirty";
-    	}if(n == 4) {
+    	}if(n%10 == 4) {
     		return "fourty";
-    	}if(n == 5) {
+    	}if(n%10 == 5) {
     		return "fifty";
-    	}if(n == 6) {
+    	}if(n%10 == 6) {
     		return "sixty";
-    	}if(n == 7) {
+    	}if(n%10 == 7) {
     		return "seventy";
-    	}if(n == 8) {
+    	}if(n%10 == 8) {
     		return "eighty";
-    	}if(n == 9) {
+    	}if(n%10 == 9) {
     		return "ninety";
     	}
     	return "";
+    }
+    
+    static String number11To19(int n) {
+    	if(n ==11){
+    		return "eleven";
+    	}if(n ==12){
+    		return "twelve";
+    	}if(n ==13){
+    		return "thirteen";
+    	}if(n ==14){
+    		return "fourteen";
+    	}if(n ==15){
+    		return "fifteen";
+    	}if(n ==16){
+    		return "sixteen";
+    	}if(n ==17){
+    		return "seventeen";
+    	}if(n ==18){
+    		return "eighteen";
+    	}if(n ==19){
+    		return "nineteen";    		
+    	}
+    	return"";
+    }
+    static String number100(int n) {
+    	if(n ==1){
+    		return "hundred";
+    	}else{
+    		return "hundreds";
+    	}
     }
 }
--- src/ite/IntToEngTest.java
+++ src/ite/IntToEngTest.java
@@ -13,6 +13,7 @@ public class IntToEngTest {
 		IntToEng ite = new IntToEng();
 		String expected = "one";
 		String actual = ite.number(1);
+		assertThat(actual,is(expected));
 	}
 	
 	@Test
@@ -20,12 +21,34 @@ public class IntToEngTest {
 		IntToEng ite = new IntToEng();
 		String expected = "ten";
 		String actual = ite.number10(1);
+		assertThat(actual,is(expected));
 	}
 	
 	@Test
 	public void translateEngメソッドで正しいアルファベットが取得できる() {
 		IntToEng ite = new IntToEng();
-		String expected = "twenty one";
-		String actual = ite.translateEng(21);
+		String expected1 = "twenty one";
+		String actual1 = ite.translateEng(21);
+		assertThat(actual1,is(expected1));
+		
+		String expected2 = "nine";
+		String actual2 = ite.translateEng(9);
+		assertThat(actual2,is(expected2));
+		
+		String expected3 = "one hundred twenty two";
+		String actual3 = ite.translateEng(122);
+		assertThat(actual3,is(expected3));
+		
+		String expected4 = "three hundreds sixty";
+		String actual4 = ite.translateEng(360);
+		assertThat(actual4,is(expected4));
+		
+		String expected5 = "one hundred";
+		String actual5 = ite.translateEng(100);
+		assertThat(actual5,is(expected5));
+		
+		String expected6 = "twelve";
+		String actual6 = ite.translateEng(12);
+		assertThat(actual6,is(expected6));
 	}
 }
